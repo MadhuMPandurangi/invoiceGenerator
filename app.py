5f34cfca22ebc8a56b779725db0ab632755e1453
MadhuMPandurangi <madhumpandurangi@gmail.com>
2024-08-06 23:43:29 +0530

    Add date dynamically

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
-from weasyprint import HTML
-html = HTML('invoice.html')
-html.write_pdf('invoice.pdf')
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    today = datetime.today().strftime("%B %-d, %Y")
+    return render_template('invoice.html',
+                            date = today)
+
+if __name__ == '__main__':
+    app.run(debug=True)
